c91559c33fc6672e92c473a3efe657bece9fddfd
Patrick Zeinert <patrick@bryanbroadcasting.com>
2012-09-05 17:29:05 -0500

    Format page.php to match index.php

--- page.php
+++ page.php
@@ -13,19 +13,29 @@
 
 get_header(); ?>
 
-		<div id="primary" class="site-content">
-			<div id="content" role="main">
+    <div id="primary" class="site-content">
+        <div id="content" role="main">
 
-				<?php while ( have_posts() ) : the_post(); ?>
+            <?php while ( have_posts() ) : the_post(); ?>
 
-					<?php get_template_part( 'content', 'page' ); ?>
+                <?php get_template_part( 'content', 'page' ); ?>
 
-					<?php comments_template( '', true ); ?>
+                <?php comments_template( '', true ); ?>
 
-				<?php endwhile; // end of the loop. ?>
+            <?php endwhile; // end of the loop. ?>
 
-			</div><!-- #content -->
-		</div><!-- #primary .site-content -->
-
-<?php get_sidebar(); ?>
+        </div><!-- #content -->
+        <aside class="aside1">
+            <?php dynamic_sidebar('Primary'); ?>
+        </aside>
+        <aside class="aside2">
+            <?php dynamic_sidebar('Secondary'); ?>
+        </aside>
+        <footer class="footer1">
+            A footer.
+        </footer>
+        <footer class="footer2">
+            Another footer.
+        </footer>
+    </div><!-- #primary .site-content -->
 <?php get_footer(); ?>
