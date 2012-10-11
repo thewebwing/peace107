742a69e2c276ff872fc5dd95bf079b75b193d261
Patrick Zeinert <patrick@bryanbroadcasting.com>
2012-10-11 18:23:44 -0500

    Swapped ad code and added css fix for LT-IE9

--- header.php
+++ header.php
@@ -37,6 +37,7 @@
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 <!--[if lt IE 9]>
 <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
+
 <![endif]-->
 
 <?php wp_head(); ?>
@@ -50,7 +51,7 @@
 
 	
 	<!--[if lt IE 9]>
-	<link rel="stylesheet" href="wp-content/themes/peace107/style/desktop.css" media="screen">
+	<link rel="stylesheet" href="wp-content/themes/peace107/style/ie-fix.css" media="screen">
 	<![endif]-->
 		
 	<!--[if (lt IE 9) & (!IEMobile)]>
@@ -63,7 +64,7 @@
 <body <?php body_class('cf'); ?>>
 <div id="page" class="hfeed site cf">
 	<?php do_action( 'before' ); ?>
-        <div class="ad leaderboard"><a href=""><img src="http://brazosvalleybride.com/assets/728x90.gif" width="728" height="90"></a></div>
+        <div class="ad leaderboard"><a href="http://www.st-joseph.org/body.cfm?id=361" target="_blank"><img class="phantom" src="http://peace107.com/wp-content/themes/peace107/style/images/phantom.gif"></a></div>
 	<header id="masthead" class="site-header" role="banner">
         <span id="branding"><a href="<?php echo site_url(); ?>"><img src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="" /></a></span>
 		<hgroup>
