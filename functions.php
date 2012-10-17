426ef1452e7bfa8479051dceb7f26786280a91fb
Patrick Zeinert <patrick@bryanbroadcasting.com>
2012-10-17 17:15:55 -0500

    Moved Morning Verse Widget to seperate file

--- functions.php
+++ functions.php
@@ -12,11 +12,9 @@
 
 $current_plugins = get_option('active_plugins');
 if (in_array('developers-custom-fields/slt-custom-fields.php', $current_plugins) && is_admin()) {
-
-
-
     include('cpt-admin-functions.php');
 }
+include('peace-widgets.php');
  
 /**
  * Set the content width based on the theme's design and stylesheet.
@@ -108,6 +106,7 @@ function peace107_widgets_init() {
         'before_title' => '<h1 class="widget-title">',
         'after_title' => '</h1>',
     ) );
+    register_widget('Peace_Widget_Morning_Verse');
 }
 add_action( 'widgets_init', 'peace107_widgets_init' );
 
@@ -127,7 +126,7 @@ function peace107_scripts() {
     wp_enqueue_script( 'helper', get_template_directory_uri() . '/js/helper.js', array( 'jquery' ), '201200917', true );
     wp_enqueue_script( 'plugins', get_template_directory_uri() . '/js/plugins.js', array( 'jquery' ), '201200917', true );
     wp_enqueue_script( 'modernizr', get_stylesheet_directory_uri() . '/libs/foundation3/javascripts/modernizr.foundation.js', array('jquery') );
-
+    //wp_enqueue_script('dfp', get_stylesheet_directory_uri() . '/js/dfp.js', array('jquery'), false, true);
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
@@ -203,103 +202,4 @@ function phantom_image_spacer() {
 add_action( 'admin_head', 'my_website_admin_head' );
 function my_website_admin_head() {
     wp_dequeue_script( 'custom-post-type-onomies-admin-options-validate' );
-}
-/**
- * Recent_Posts widget class
- *
- * @since 2.8.0
- */
-class WP_Widget_Morning_Verse extends WP_Widget {
-
-	function __construct() {
-		$widget_ops = array('classname' => 'widget_morning_verse_entries', 'description' => __( "Display's latest post in the category \"Morning Verse\"") );
-		parent::__construct('morning-verse', __('Morning Verse'), $widget_ops);
-		$this->alt_option_name = 'widget_morning_verse_entries';
-
-		add_action( 'save_post', array(&$this, 'flush_widget_cache') );
-		add_action( 'deleted_post', array(&$this, 'flush_widget_cache') );
-		add_action( 'switch_theme', array(&$this, 'flush_widget_cache') );
-	}
-
-	function widget($args, $instance) {
-		$cache = wp_cache_get('widget_recent_morning_verse', 'widget');
-
-		if ( !is_array($cache) )
-			$cache = array();
-
-		if ( ! isset( $args['widget_id'] ) )
-			$args['widget_id'] = $this->id;
-
-		if ( isset( $cache[ $args['widget_id'] ] ) ) {
-			echo $cache[ $args['widget_id'] ];
-			return;
-		}
-
-		ob_start();
-		extract($args);
-
-		$title = apply_filters('widget_title', empty($instance['title']) ? __('Morning Verse') : $instance['title'], $instance, $this->id_base);
-		if ( empty( $instance['number'] ) || ! $number = absint( $instance['number'] ) )
- 			$number = 10;
-		$show_date = isset( $instance['show_date'] ) ? $instance['show_date'] : false;
-
-		$r = new WP_Query( apply_filters( 'widget_posts_args', array( 'posts_per_page' => $number, 'no_found_rows' => true, 'post_status' => 'publish', 'ignore_sticky_posts' => true ) ) );
-		if ($r->have_posts()) :
-?>
-		<?php echo $before_widget; ?>
-		<?php if ( $title ) echo $before_title . $title . $after_title; ?>
-		<ul>
-		<?php while ( $r->have_posts() ) : $r->the_post(); ?>
-			<li>
-				<a href="<?php the_permalink() ?>" title="<?php echo esc_attr( get_the_title() ? get_the_title() : get_the_ID() ); ?>"><?php if ( get_the_title() ) the_title(); else the_ID(); ?></a>
-			<?php if ( $show_date ) : ?>
-				<span class="post-date"><?php echo get_the_date(); ?></span>
-			<?php endif; ?>
-			</li>
-		<?php endwhile; ?>
-		</ul>
-		<?php echo $after_widget; ?>
-<?php
-		// Reset the global $the_post as this query will have stomped on it
-		wp_reset_postdata();
-
-		endif;
-
-		$cache[$args['widget_id']] = ob_get_flush();
-		wp_cache_set('widget_recent_morning_verse', $cache, 'widget');
-	}
-
-	function update( $new_instance, $old_instance ) {
-		$instance = $old_instance;
-		$instance['title'] = strip_tags($new_instance['title']);
-		$instance['number'] = (int) $new_instance['number'];
-		$instance['show_date'] = (bool) $new_instance['show_date'];
-		$this->flush_widget_cache();
-
-		$alloptions = wp_cache_get( 'alloptions', 'options' );
-		if ( isset($alloptions['widget_morning_verse_entries']) )
-			delete_option('widget_morning_verse_entries');
-
-		return $instance;
-	}
-
-	function flush_widget_cache() {
-		wp_cache_delete('widget_recent_morning_verse', 'widget');
-	}
-
-	function form( $instance ) {
-		$title     = isset( $instance['title'] ) ? esc_attr( $instance['title'] ) : '';
-		$number    = isset( $instance['number'] ) ? absint( $instance['number'] ) : 5;
-		$show_date = isset( $instance['show_date'] ) ? (bool) $instance['show_date'] : false;
-?>
-		<p><label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label>
-		<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo $title; ?>" /></p>
-
-		<p><label for="<?php echo $this->get_field_id( 'number' ); ?>"><?php _e( 'Number of posts to show:' ); ?></label>
-		<input id="<?php echo $this->get_field_id( 'number' ); ?>" name="<?php echo $this->get_field_name( 'number' ); ?>" type="text" value="<?php echo $number; ?>" size="3" /></p>
-
-		<p><input class="checkbox" type="checkbox" <?php checked( $show_date ); ?> id="<?php echo $this->get_field_id( 'show_date' ); ?>" name="<?php echo $this->get_field_name( 'show_date' ); ?>" />
-		<label for="<?php echo $this->get_field_id( 'show_date' ); ?>"><?php _e( 'Display post date?' ); ?></label></p>
-<?php
-	}
 }
